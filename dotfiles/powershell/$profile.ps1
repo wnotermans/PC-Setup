6a3237c467469b5b05901e8bb63701b8f6c462e4
wnotermans <woutnotermans1@gmail.com>
2025-03-25 18:05:36 +0100

    ✨ feat(PS): Add new command line functions
    The no-metadata allows downloading of non-"Topic" music videos.

--- dotfiles/powershell/$profile.ps1
+++ dotfiles/powershell/$profile.ps1
@@ -31,3 +31,5 @@ $ENV:STARSHIP_CONFIG = "$env:USERPROFILE\.config\starship\starship.toml"
 # yt-dlp functions to download music from youtube
 function dlp { yt-dlp --extract-audio --audio-format mp3 --audio-quality 128k --embed-thumbnail --embed-metadata --parse-metadata "%(release_year)s0823:%(upload_date)s" --output "%(title)s.%(ext)s" $args }
 function dlpl { yt-dlp --extract-audio --audio-format mp3 --audio-quality 128k --embed-thumbnail --embed-metadata --parse-metadata "%(release_year)s0823:%(upload_date)s" --yes-playlist --output "%(title)s.%(ext)s" $args }
+function dlp-nm { yt-dlp --extract-audio --audio-format mp3 --audio-quality 128k --embed-thumbnail --output "%(title)s.%(ext)s" $args }
+function dlpl-nm { yt-dlp --extract-audio --audio-format mp3 --audio-quality 128k --embed-thumbnail --yes-playlist --output "%(title)s.%(ext)s" $args }
